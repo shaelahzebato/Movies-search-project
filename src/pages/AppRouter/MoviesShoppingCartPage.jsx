4a1ff7d0b11a07aebe0656fd0d93707fb12fa634
Shaelah Zebato <nadiakareneshaelah@gmail.com>
2024-08-23 20:54:37 +0000

    premier commit

--- src/pages/AppRouter/MoviesShoppingCartPage.jsx
+++ src/pages/AppRouter/MoviesShoppingCartPage.jsx
@@ -1,10 +1,31 @@
 import { faArrowRight, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import NavBar from '../../components/NavBar/NavBar'
 
 function MovieShoppingCartPage() {
+
+    const [moviesShopCart, setMoviesShopCart]= useState([])
+
+    const imgBase = "https://image.tmdb.org/t/p/original/";
+
+    const getMoviesForShopCart = () => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=a7370479d17c1c001f3a2bb1dc10dd53')
+        .then(response => response.json())
+        .then(data => {
+            const getResult = data.results;
+            const dataSliced = getResult.slice(0, 3)
+            setMoviesShopCart(dataSliced)
+        })
+        .catch(error => console.error(error));
+    }
+
+    useEffect(() => {
+        getMoviesForShopCart()
+    })
+
+
     return (
         <>
             <div className='bg-black bg-opacity-85'>
@@ -33,37 +54,42 @@ function MovieShoppingCartPage() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="flex flex-col min-[500px]:flex-row min-[500px]:items-center gap-5 py-6  border-b border-gray-200 group">
-                                <div className="w-full md:max-w-[126px]">
-                                    <img src="https://pagedone.io/asset/uploads/1701162850.png" alt="perfume bottle" className="mx-auto rounded-xl"/>
-                                </div>
-                                <div className="grid grid-cols-1 md:grid-cols-4 w-full">
-                                    <div className="md:col-span-2">
-                                        <div className="flex flex-col max-[500px]:items-center gap-3">
-                                            <h6 className="font-semibold text-base leading-7 text-black">Rose Petals Divine</h6>
-                                            <h6 className="font-normal text-base leading-7 text-gray-500">Parfum</h6>
-                                            <h6 className="font-medium text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-orange-600">$120.00</h6>
+                            {
+                                moviesShopCart.map((cart, index) => (
+                                    <div key={index} className="flex flex-col min-[500px]:flex-row min-[500px]:items-center gap-5 py-6  border-b border-gray-200 group">
+                                        <div className="w-full md:max-w-[126px]">
+                                            <img src={`${imgBase}${cart?.backdrop_path}`} alt="perfume bottle" className="mx-auto rounded-xl"/>
+                                        </div>
+                                        <div className="grid grid-cols-1 md:grid-cols-4 w-full">
+                                            <div className="md:col-span-2">
+                                                <div className="flex flex-col max-[500px]:items-center gap-3">
+                                                    <h6 className="font-semibold text-base leading-7 text-black">{cart.title}</h6>
+                                                    <h6 className="font-normal text-base leading-7 text-gray-500">Serie</h6>
+                                                    <h6 className="font-medium text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-orange-600">$120.00</h6>
+                                                </div>
+                                            </div>
+                                            <div className="flex items-center max-[500px]:justify-center h-full max-md:mt-3">
+                                                <div className="flex items-center h-full">
+                                                    <button className="group rounded-l-xl px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300">
+                                                        <FontAwesomeIcon className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black" icon={faMinus}/>
+                                                    </button>
+                                                    <input type="text"
+                                                        className="border-y border-gray-200 outline-none text-gray-900 font-semibold text-lg w-full max-w-[73px] min-w-[60px] placeholder:text-gray-900 py-[12px]  text-center bg-transparent"
+                                                        placeholder="1"/>
+                                                    <button className="group rounded-r-xl px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300">
+                                                        <FontAwesomeIcon className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black" icon={faPlus}/>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <div className="flex items-center max-[500px]:justify-center md:justify-end max-md:mt-3 h-full">
+                                                <p className="font-bold text-lg leading-8 text-gray-600 text-center transition-all duration-300 group-hover:text-orange-600">$120.00</p>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="flex items-center max-[500px]:justify-center h-full max-md:mt-3">
-                                        <div className="flex items-center h-full">
-                                            <button className="group rounded-l-xl px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300">
-                                                <FontAwesomeIcon className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black" icon={faMinus}/>
-                                            </button>
-                                            <input type="text"
-                                                className="border-y border-gray-200 outline-none text-gray-900 font-semibold text-lg w-full max-w-[73px] min-w-[60px] placeholder:text-gray-900 py-[12px]  text-center bg-transparent"
-                                                placeholder="1"/>
-                                            <button className="group rounded-r-xl px-5 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-50 hover:border-gray-300 hover:shadow-gray-300 focus-within:outline-gray-300">
-                                                <FontAwesomeIcon className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black" icon={faPlus}/>
-                                            </button>
-                                        </div>
-                                    </div>
-                                    <div className="flex items-center max-[500px]:justify-center md:justify-end max-md:mt-3 h-full">
-                                        <p className="font-bold text-lg leading-8 text-gray-600 text-center transition-all duration-300 group-hover:text-orange-600">$120.00</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex flex-col min-[500px]:flex-row min-[500px]:items-center gap-5 py-6  border-b border-gray-200 group">
+                                ))
+                            }
+
+                            {/* <div className="flex flex-col min-[500px]:flex-row min-[500px]:items-center gap-5 py-6  border-b border-gray-200 group">
                                 <div className="w-full md:max-w-[126px]">
                                     <img src="https://pagedone.io/asset/uploads/1701162866.png" alt="perfume bottle" className="mx-auto rounded-xl"/>
                                 </div>
@@ -93,8 +119,6 @@ function MovieShoppingCartPage() {
                                     </div>
                                 </div>
                             </div>
-
-
                             <div className="flex flex-col min-[500px]:flex-row min-[500px]:items-center gap-5 py-6  border-b border-gray-200 group">
                                 <div className="w-full md:max-w-[126px]">
                                     <img src="https://pagedone.io/asset/uploads/1701162880.png" alt="perfume bottle" className="mx-auto rounded-xl"/>
@@ -125,7 +149,7 @@ function MovieShoppingCartPage() {
                                         <p className="font-bold text-lg leading-8 text-gray-600 text-center transition-all duration-300 group-hover:text-orange-600">$120.00</p>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
 
 
                             <div className="flex items-center justify-end mt-8">
