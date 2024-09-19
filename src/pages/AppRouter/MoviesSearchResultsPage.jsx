3644f79e7305fb83a3cb70fb71ae8e4efd486e59
Shaelah Zebato <nadiakareneshaelah@gmail.com>
2024-09-19 21:10:54 +0000

    Optimisation du code

--- src/pages/AppRouter/MoviesSearchResultsPage.jsx
+++ src/pages/AppRouter/MoviesSearchResultsPage.jsx
@@ -20,10 +20,7 @@ function MovieSearchResultsPage() {
     const [movieFetchData, setMovieFetchData] = useState([])
 
     const [cart, setCart] = useState([]);
-
-    const [errors, setErrors] = useState({
-        moviename: ""
-    });
+    const [errors, setErrors] = useState({ moviename: "" });
 
 
     const handleChangeInput = (e) => {
@@ -37,34 +34,23 @@ function MovieSearchResultsPage() {
         })
     }, [name])
 
-    // const searchMovies = (e) => {
-    //     e.preventDefault()
-    //     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(inputValue)}`;
-    //     fetch(url).then((response) => response.json()).then((data) => {
-    //         setMovieFetchData(data.results)
-    //     })
-    // }
-
     const searchMovies = (e) => {
-        e.preventDefault()
-
+        e.preventDefault();
         let newErrors = {};
 
-        if(!inputValue) {
+        if (!inputValue) {
             newErrors.moviename = "Le nom d'un film est requis.";
             setErrors(newErrors);
-        }
-        else if(inputValue.length < 3) {
+        } else if (inputValue.length < 3) {
             newErrors.moviename = "Le nom d'un film doit contenir au moins 3 caractères.";
             setErrors(newErrors);
-        }
-        else if(inputValue.length >= 3) {
+        } else {
             const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(inputValue)}`;
             fetch(url).then((response) => response.json()).then((data) => {
-                setMovieFetchData(data.results)
-            })
+                setMovieFetchData(data.results);
+            });
         }
-    }
+    };
 
     const addToCart = (movieId) => {
         // Trouvez le film correspondant avec movieId
@@ -77,83 +63,58 @@ function MovieSearchResultsPage() {
 
 
     return (
-        // <div className='min-h-screen bg-cover bg-center' style={{backgroundImage:`url(${searchImg})`}}>
-        //     <div className="container mx-auto flex flex-col py-10 gap-10 px-4 lg:px-0">
-        //         <NavBar/>
-        //         <div className="flex flex-col gap-4">
-        //             <form onSubmit={(e) => searchMovies(e)} className='lg:w-[35%] flex gap-2'>
-        //                 <input value={inputValue} onChange={handleChangeInput} className='w-full p-2 rounded-sm focus:outline-none border-none' type="text" name="" id="" />
-        //                 <button className='bg-orange-500 text-white px-4 rounded-sm'>Rechercher</button>
-        //             </form>
-        //             {errors.moviename && <p className="text-red-500 text-xs italic">{errors.moviename}</p>}
-        //             <span className='text-gray-300'>{movieFetchData.length} résultats de la recherche.</span>
-        //         </div>
-        //         {name && (
-        //             <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 text-white">
-        //                 {movieFetchData.map((movie, index) => (
-        //                     <Link to={`/movie-details?id=${movie.id}`} key={index} className="relative w-full h-64 bg-gray-600">
-        //                         <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} alt="fond" className="w-full h-full object-cover"/>
-        //                         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        //                             <h1 className="text-white text-center text-xl font-bold Montserrat">{movie.title}</h1>
-        //                         </div>
-        //                     </Link>
-        //                 ))}
-        //             </div>
-        //         )}
-        //     </div>
-        // </div>
         <div className='min-h-screen bg-cover bg-center' style={{ backgroundImage: `url(${searchImg})` }}>
-    <div className="container mx-auto flex flex-col py-10 gap-10 px-4 lg:px-0">
-        <NavBar />
-        <div className="flex flex-col gap-4">
-            <form onSubmit={(e) => searchMovies(e)} className='lg:w-[35%] flex gap-2'>
-                <input 
-                    value={inputValue} 
-                    onChange={handleChangeInput} 
-                    className='w-full p-3 rounded-sm focus:outline-none border-2 border-gray-300 bg-white shadow-md transition duration-300 ease-in-out focus:border-orange-500' 
-                    type="text" 
-                    placeholder="Recherchez un film..."
-                />
-                <button 
-                    type="submit" 
-                    className='bg-orange-500 text-white px-6 py-3 rounded-sm shadow-md transition duration-300 ease-in-out hover:bg-orange-600 focus:outline-none'
-                >
-                    Rechercher
-                </button>
-            </form>
-            {errors.moviename && <p className="text-red-500 text-xs italic">{errors.moviename}</p>}
-            <span className='text-gray-300'>{movieFetchData.length} résultats de la recherche.</span>
-        </div>
-        {name && (
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-white">
-                {movieFetchData.map((movie, index) => (
-                    <div key={index} className="relative group bg-gray-600 rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
-                        <Link to={`/movie-details?id=${movie.id}`}>
-                            <img 
-                                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} 
-                                alt={movie.title} 
-                                className="w-full h-64 object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
-                            />
-                            <div className="absolute inset-0 flex flex-col justify-end p-4 bg-gradient-to-t from-black to-transparent transition-opacity duration-300 ease-in-out group-hover:opacity-100 opacity-80">
-                                <h1 className="text-white text-lg font-bold Montserrat">{movie.title}</h1>
-                                <div className="flex justify-between items-center mt-2">
-                                    <span className="text-orange-400 text-sm font-semibold">${movie.price || 'N/A'}</span>
-                                    <button 
-                                        onClick={() => addToCart(movie.id)} 
-                                        className="bg-orange-500 text-white px-4 py-2 rounded-full text-sm transition duration-300 ease-in-out hover:bg-orange-600 focus:outline-none"
-                                    >
-                                        Ajouter au panier
-                                    </button>
-                                </div>
+            <div className="container mx-auto flex flex-col py-10 gap-10 px-4 lg:px-0">
+                <NavBar />
+                <div className="flex flex-col gap-4">
+                    <form onSubmit={(e) => searchMovies(e)} className='lg:w-[35%] flex gap-2'>
+                        <input 
+                            value={inputValue} 
+                            onChange={handleChangeInput} 
+                            className='w-full p-3 rounded-sm focus:outline-none border-2 border-gray-300 bg-white shadow-md transition duration-300 ease-in-out focus:border-orange-500' 
+                            type="text" 
+                            placeholder="Recherchez un film..."
+                        />
+                        <button 
+                            type="submit" 
+                            className='bg-orange-500 text-white px-6 py-3 rounded-sm shadow-md transition duration-300 ease-in-out hover:bg-orange-600 focus:outline-none'
+                        >
+                            Rechercher
+                        </button>
+                    </form>
+                    {errors.moviename && <p className="text-red-500 text-xs italic">{errors.moviename}</p>}
+                    <span className='text-gray-300'>{movieFetchData.length} résultats de la recherche.</span>
+                </div>
+                {name && (
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-white">
+                        {movieFetchData.map((movie, index) => (
+                            <div key={index} className="relative group bg-gray-600 rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
+                                <Link to={`/movie-details?id=${movie.id}`}>
+                                    <img 
+                                        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} 
+                                        alt={movie.title} 
+                                        className="w-full h-64 object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
+                                    />
+                                    <div className="absolute inset-0 flex flex-col justify-end p-4 bg-gradient-to-t from-black to-transparent transition-opacity duration-300 ease-in-out group-hover:opacity-100 opacity-80">
+                                        <h1 className="text-white text-lg font-bold Montserrat">{movie.title}</h1>
+                                        <div className="flex justify-between items-center mt-2">
+                                            <span className="text-orange-400 text-sm font-semibold">${movie.price || '100'}</span>
+                                            <button 
+                                                onClick={() => addToCart(movie.id)} 
+                                                className="flex items-center justify-center gap-2 bg-orange-500 text-white px-4 py-2 rounded-full text-sm transition duration-300 ease-in-out hover:bg-orange-600 focus:outline-none"
+                                            >
+                                                <FontAwesomeIcon icon={faPlus}/>
+                                                <span>Panier</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </Link>
                             </div>
-                        </Link>
+                        ))}
                     </div>
-                ))}
+                )}
             </div>
-        )}
-    </div>
-</div>
-
+        </div>
     )
 }
 
--- src/pages/AppRouter/MoviesShoppingCartPage.jsx
+++ src/pages/AppRouter/MoviesShoppingCartPage.jsx
@@ -42,8 +42,7 @@ function MovieShoppingCartPage() {
 
     return (
         <>
-            {/* Revenir à l'ancien */}
-            {/* <div className='bg-black bg-opacity-85 z-50'>
+            <div className='bg-black bg-opacity-85 z-50'>
                 <NavBar/>
             </div>
             <section className="relative z-10 after:contents-[''] after:absolute after:z-0 after:h-full xl:after:w-1/3 after:top-0 after:right-0 after:bg-gray-50">
@@ -103,7 +102,6 @@ function MovieShoppingCartPage() {
                                     </div>
                                 ))
                             }
-
                             <div className="flex items-center justify-end mt-8">
                                 <Link to={"/movies-results"} className="flex items-center px-5 py-3 rounded-full gap-2 border-none outline-0 group font-semibold text-lg leading-8 text-orange-500 shadow-sm shadow-transparent transition-all duration-500 hover:text-orange-600 italic">
                                     Acheter à nouveau
@@ -138,92 +136,7 @@ function MovieShoppingCartPage() {
                         </div>
                     </div>
                 </div>
-            </section> */}
-            <div className='bg-black bg-opacity-85 z-50'>
-                <NavBar/>
-            </div>
-
-            <section className="relative z-10">
-                <div className="w-full max-w-7xl px-4 md:px-6 lg:px-8 mx-auto relative z-10">
-                    <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
-                        <div className="col-span-1 md:col-span-8 pt-14 pb-8 lg:py-24 w-full">
-                            <div className="flex items-center justify-between pb-8 border-b border-gray-300">
-                                <h2 className="text-3xl font-bold text-orange-500">Panier d'achat(s)</h2>
-                                <h2 className="text-xl font-bold text-gray-600">3 article(s)</h2>
-                            </div>
-
-                            <div className="hidden md:grid grid-cols-12 gap-4 mt-8 pb-6 border-b border-gray-200">
-                                <div className="col-span-7">
-                                    <p className="text-lg text-gray-400">Détails des produits</p>
-                                </div>
-                                <div className="col-span-5">
-                                    <div className="grid grid-cols-5 gap-2">
-                                        <div className="col-span-3 text-center">
-                                            <p className="text-lg text-gray-400">Quantité</p>
-                                        </div>
-                                        <div className="col-span-2 text-center">
-                                            <p className="text-lg text-gray-400">Total</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {moviesShopCart.map((cart, index) => (
-                                <div key={index} className="flex flex-col md:flex-row items-center gap-4 py-6 border-b border-gray-200">
-                                    <div className="w-full md:w-1/4">
-                                        <img src={`${imgBase}${cart?.backdrop_path}`} alt={cart.title} className="w-full rounded-xl"/>
-                                    </div>
-                                    <div className="flex-1 md:flex md:justify-between md:items-center">
-                                        <div className="flex flex-col gap-2">
-                                            <h6 className="text-base font-semibold text-black">{cart.title}</h6>
-                                            <p className="text-base text-gray-500">Série</p>
-                                            <p className="text-base font-medium text-gray-600">$120.00</p>
-                                        </div>
-                                        <div className="flex items-center gap-2 mt-4 md:mt-0">
-                                            <button onClick={(e) => decrementQuantity(e)} className="px-4 py-2 border border-gray-200 rounded-lg hover:bg-gray-100">
-                                                <FontAwesomeIcon icon={faMinus}/>
-                                            </button>
-                                            <input type="text" className="border border-gray-200 text-gray-900 font-semibold text-lg w-16 text-center bg-transparent rounded" value={quantity}/>
-                                            <button onClick={(e) => incrementQuantity(e)} className="px-4 py-2 border border-gray-200 rounded-lg hover:bg-gray-100">
-                                                <FontAwesomeIcon icon={faPlus}/>
-                                            </button>
-                                        </div>
-                                        <p className="text-lg font-bold text-gray-600 md:text-right mt-4 md:mt-0">$120.00</p>
-                                    </div>
-                                </div>
-                            ))}
-
-                            <div className="flex items-center justify-end mt-8">
-                                <Link to={"/movies-results"} className="inline-flex items-center px-5 py-3 rounded-full border-none font-semibold text-lg text-orange-500 transition duration-300 hover:text-orange-600">
-                                    Acheter à nouveau
-                                    <FontAwesomeIcon className="ml-2 transition-transform transform hover:translate-x-1" icon={faArrowRight}/>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="col-span-1 md:col-span-4 bg-gray-50 p-6 rounded-lg">
-                            <h2 className="text-3xl font-bold text-black pb-6 border-b border-gray-300">Résumé de la commande</h2>
-                            <div className="mt-6">
-                                <div className="flex justify-between pb-4">
-                                    <p className="text-lg text-black">3 article(s)</p>
-                                    <p className="text-lg font-medium text-black">$480.00</p>
-                                </div>
-                                <form>
-                                    <label className="block text-gray-400 text-sm font-medium mb-2">Code promo</label>
-                                    <input type="text" className="w-full h-11 px-4 text-base text-gray-900 bg-white border border-gray-300 rounded-lg placeholder-gray-500" placeholder="XXXX XXXX XXXX"/>
-                                    <button className="mt-4 w-full bg-black py-2 px-4 text-white text-sm font-semibold rounded-lg hover:bg-black/80 transition duration-300">Appliquer</button>
-                                    <div className="flex justify-between py-6 mt-4">
-                                        <p className="text-xl font-medium text-black">Total</p>
-                                        <p className="text-xl font-semibold text-orange-600">$485.00</p>
-                                    </div>
-                                    <button className="w-full bg-orange-500 py-3 px-6 text-white text-lg font-semibold rounded-xl hover:bg-orange-600 transition duration-300">Payer</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </section>
-
         </>
     )
 }
